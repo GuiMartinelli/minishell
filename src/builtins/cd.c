f99d84b1551695b106e0ecea5a0d2427d33a2582
Paulo Marques <paulormarques21@gmail.com>
2021-12-20 17:05:03 -0300

    feat(builtins): add directory shortcuts
    To $HOME (NULL and '--')
    To $OLDPWD ('-')

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: proberto <proberto@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/29 15:39:20 by proberto          #+#    #+#             */
-/*   Updated: 2021/12/10 21:43:28 by proberto         ###   ########.fr       */
+/*   Updated: 2021/12/20 15:26:43 by proberto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,13 @@ void	cd(char *dir, t_var *env_list)
 	char	*pwd;
 	char	*oldpwd;
 
+	if (*dir == NULL || (*dir == '-' && *(dir + 1) == '-' && *(dir + 2) == 0))
+		dir = get_var_value("HOME", env_list);
+	else if (*dir == '-' && *(dir + 1) == NULL)
+	{
+		dir = get_var_value("OLDPWD", env_list);
+		ft_putendl_fd(dir, 1);
+	}
 	if (chdir(dir))
 	{
 		perror("chdir() failed");
