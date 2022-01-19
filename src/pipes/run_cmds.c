12da995d366831cabf14102d7b654b056fb8d261
Paulo Marques <paulormarques21@gmail.com>
2022-01-19 20:12:59 -0300

    feat(pipes): add builtins options

--- src/pipes/run_cmds.c
+++ src/pipes/run_cmds.c
@@ -6,7 +6,7 @@
 /*   By: proberto <proberto@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/17 20:02:27 by guferrei          #+#    #+#             */
-/*   Updated: 2022/01/19 18:49:12 by proberto         ###   ########.fr       */
+/*   Updated: 2022/01/19 20:10:37 by proberto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ void	free_cmd(t_cmd *cmd)
 	free(cmd);
 }
 
-void	run_cmds(char **matrix, char **envp, int input, int output)
+void	run_cmds(char **matrix, char **envp, int input, int output, t_var *env_list)
 {
 	t_cmd	*cmd;
 	int		fd[2];
@@ -42,45 +42,59 @@ void	run_cmds(char **matrix, char **envp, int input, int output)
 	if (*matrix && **matrix == '|')
 	{
 		pipe(fd);
-		pid = fork();
-		signal(SIGINT, interrupt_process);
-		signal(SIGQUIT, quit_process);
-		if (pid == 0)
+		if (launch_builtins(cmd->option[0], cmd->option, env_list, fd[1]))
 		{
-			dup2(input, 0);
-			dup2(fd[1], 1);
-			if (input != 0)
-				close(input);
 			close(fd[1]);
-			execve(cmd->name, cmd->option, cmd->env);
+			matrix++;
+			run_cmds(matrix, envp, fd[0], output, env_list);
 		}
 		else
 		{
-			wait(NULL);
-			close(fd[1]);
-			matrix++;
-			run_cmds(matrix, envp, fd[0], output);
-			close(fd[0]);
+			pid = fork();
+			signal(SIGINT, interrupt_process);
+			signal(SIGQUIT, quit_process);
+			if (pid == 0)
+			{
+				dup2(input, 0);
+				dup2(fd[1], 1);
+				if (input != 0)
+					close(input);
+				close(fd[1]);
+				execve(cmd->name, cmd->option, cmd->env);
+			}
+			else
+			{
+				wait(NULL);
+				close(fd[1]);
+				matrix++;
+				run_cmds(matrix, envp, fd[0], output, env_list);
+				close(fd[0]);
+			}
 		}
 	}
 	else
 	{
-		pid = fork();
-		signal(SIGINT, interrupt_process);
-		signal(SIGQUIT, quit_process);
-		if (pid == 0)
-		{
-			dup2(input, 0);
-			dup2(output, 1);
-			execve(cmd->name, cmd->option, cmd->env);
-			if (input != 0)
-				close(input);
-		}
+		if (launch_builtins(cmd->option[0], cmd->option, env_list, output))
+			;
 		else
 		{
-			wait(NULL);
-			if (input != 0)
-				close(input);
+			pid = fork();
+			signal(SIGINT, interrupt_process);
+			signal(SIGQUIT, quit_process);
+			if (pid == 0)
+			{
+				dup2(input, 0);
+				dup2(output, 1);
+				execve(cmd->name, cmd->option, cmd->env);
+				if (input != 0)
+					close(input);
+			}
+			else
+			{
+				wait(NULL);
+				if (input != 0)
+					close(input);
+			}
 		}
 	}
 	free_cmd(cmd);
