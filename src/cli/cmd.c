e6a856febbda6fab42035e62a147c0e62e3d0558
Guilherme Martinelli <guilhermefmartinelli@gmail.com>
2022-01-27 10:27:24 -0300

    norminette fix

--- src/cli/cmd.c
+++ src/cli/cmd.c
@@ -6,7 +6,7 @@
 /*   By: guferrei <guferrei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/29 14:55:52 by proberto          #+#    #+#             */
-/*   Updated: 2022/01/27 10:22:32 by guferrei         ###   ########.fr       */
+/*   Updated: 2022/01/27 10:26:45 by guferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,46 +38,6 @@ int	launch_builtins(char *cmd, char **arg, t_var *env_list, int fd)
 	else
 		return (FALSE);
 	return (TRUE);
-}
-
-void	launch_execve(char *path, char **arg, char **envp, int fd[2])
-{
-	pid_t	pid;
-
-	if (access(path, F_OK) == 0)
-	{
-		pid = fork();
-		signal(SIGINT, interrupt_process);
-		signal(SIGQUIT, quit_process);
-		if (pid == 0)
-		{
-			dup2(fd[0], 0);
-			dup2(fd[1], 1);
-			if (execve(path, arg, envp) == -1)
-			{
-				perror("minishell");
-				exit(EXIT_FAILURE);
-			}
-			if (fd[0] != 0)
-				close(fd[0]);
-			if (fd[1] != 1)
-				close(fd[1]);
-			fd[0] = 0;
-		}
-		else if (pid == -1)
-			ft_putendl_fd("\nFailed forking child..", 2);
-		else
-		{
-			wait(NULL);
-			if (fd[0] != 0)
-				close(fd[0]);
-			if (fd[1] != 1)
-				close(fd[1]);
-			fd[0] = 0;
-		}
-	}
-	else
-		ft_putendl_fd("minishell: command not found: ", 2);
 }
 
 void	eval(char *command_line, t_var *env_list, char **envp)
