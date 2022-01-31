c6c6421152e639472c04bbd998dc4c6a6f4748e9
Guilherme Martinelli <guilhermefmartinelli@gmail.com>
2022-01-31 10:44:47 -0300

    refactor and removing useless variables

--- src/pipes/parse_cmds.c
+++ src/pipes/parse_cmds.c
@@ -6,7 +6,7 @@
 /*   By: guferrei <guferrei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 18:58:44 by guferrei          #+#    #+#             */
-/*   Updated: 2022/01/31 09:41:04 by guferrei         ###   ########.fr       */
+/*   Updated: 2022/01/31 09:51:44 by guferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,16 +25,10 @@ int	get_args_size(char **matrix)
 
 char	*parse_absolute_path(char *path)
 {
-	int		size;
-
-	size = 0;
 	while (*path)
 		path++;
 	while (*path != '/')
-	{
 		path--;
-		size++;
-	}
 	return (ft_strdup((path + 1)));
 }
 
@@ -51,7 +45,7 @@ char	**parse_cmd(t_cmd *cmd, char **matrix, char **env, t_var *env_list)
 	index = 0;
 	paths = parse_paths(env_list);
 	cmd->name = check_path(paths, matrix[0]);
-	cmd->option = malloc(get_args_size(matrix) * sizeof(char *));
+	cmd->option = ft_calloc(get_args_size(matrix), sizeof(char *));
 	while (*matrix && **matrix != '|' && **matrix != '<' && **matrix != '>' )
 	{
 		if (index == 0 && **matrix == '/')
@@ -61,7 +55,6 @@ char	**parse_cmd(t_cmd *cmd, char **matrix, char **env, t_var *env_list)
 		matrix++;
 		index++;
 	}
-	cmd->option[index] = NULL;
 	cmd->env = env;
 	if (paths)
 		free_matrix(paths);
