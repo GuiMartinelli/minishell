11e0a3d94385f20313bc52d35a0ddcf3c18113b1
Paulo Marques <paulormarques21@gmail.com>
2022-02-12 23:56:29 -0300

    feat: add syntax error to double pipe

--- src/eval/eval.c
+++ src/eval/eval.c
@@ -6,11 +6,36 @@
 /*   By: proberto <proberto@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/29 14:55:52 by proberto          #+#    #+#             */
-/*   Updated: 2022/02/06 17:23:52 by proberto         ###   ########.fr       */
+/*   Updated: 2022/02/12 23:00:23 by proberto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+
+/**
+ * @brief Check for syntax error (more than one pipe followed).
+ * 
+ * @param cl char array (acronym for command line)
+ * @return true if there is a syntax error, false otherwise
+ */
+static int	check_syntax_error(char **cl)
+{
+	size_t	i;
+
+	i = 0;
+	while (cl[i])
+	{
+		if (ft_strchr(cl[i], '|')
+			&& (cl[i + 1] && ft_strchr(cl[i + 1], '|')))
+		{
+			ft_putendl_fd("minishell: syntax error near unexpected token `|'",
+				STDERR_FILENO);
+			return (TRUE);
+		}
+		i++;
+	}
+	return (FALSE);
+}
 
 /**
  * @brief Initialize the command structure.
@@ -40,6 +65,11 @@ void	eval(char *cl, t_env_var *env)
 
 	cl_lex = ft_lexer(cl, env->list, env->envp);
 	if (!*cl_lex)
+	{
+		free_matrix(cl_lex);
+		return ;
+	}
+	if (check_syntax_error(cl_lex) == TRUE)
 	{
 		free_matrix(cl_lex);
 		return ;
